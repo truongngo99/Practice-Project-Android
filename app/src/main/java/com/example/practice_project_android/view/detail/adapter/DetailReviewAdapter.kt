e2d66809499f96af27ee36f0611355213a172f72
truongngo99 <truong.ngo@executionlab.asia>
2021-11-26 10:26:46 +0700

    get data review

--- app/src/main/java/com/example/practice_project_android/view/detail/adapter/DetailReviewAdapter.kt
+++ app/src/main/java/com/example/practice_project_android/view/detail/adapter/DetailReviewAdapter.kt
@@ -3,6 +3,8 @@ package com.example.practice_project_android.view.detail.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.practice_project_android.R
 import com.example.practice_project_android.data.model.movie.ResultReview
 import com.example.practice_project_android.databinding.ItemReviewLayoutBinding
 
@@ -21,7 +23,22 @@ class DetailReviewAdapter : RecyclerView.Adapter<DetailReviewHolder>() {
 
     override fun onBindViewHolder(holder: DetailReviewHolder, position: Int) {
         val item = data[position]
-        holder.binding.tvContent.text = item.content
+        holder.binding.apply {
+            tvContent.text = item.content
+            txtName.text = item.author_details?.name
+            tvDate.text = item.created_at
+            if (item.author_details?.rating == null){
+                tvRating.text = 0.toString()
+            } else{
+                tvRating.text = item.author_details.rating.toString()
+            }
+
+            Glide.with(root).load(item.author_details?.avatar_path)
+                .placeholder(R.drawable.default_avatar)
+                .into(imgAvatar)
+
+        }
+
     }
 
     override fun getItemCount(): Int {
